17cded7bee0bc139ff07f3d726435cbf372efec5
Pranshu Gupta <pranshug258@outlook.com>
2016-01-24 05:07:47 +0530

    Added calls to get bandwidth and seeds

--- segment.py
+++ segment.py
@@ -4,33 +4,49 @@
 
 import Image
 import numpy as np
+from sklearn.neighbors import BallTree
+from sklearn.cluster import estimate_bandwidth, get_bin_seeds
+from sklearn.utils import extmath
+from collections import defaultdict
+from sklearn.metrics.pairwise import euclidean_distances
+
 
 ###################################################################################################
 # CONSTRUCT THE IMAGE DATA MATRIX
+def get_data(img_data):
+	i = 0
+	data = []
+	for row in img_data:
+		j = 0
+		for pixel in row:
+			data.append([i, j, pixel[0], pixel[1], pixel[2]])
+			j = j + 1
+		i = i + 1
+	data = np.array(data)
+	return data
 
-# Load the image
+###################################################################################################
+# SELECT THE INITIAL KERNEL CENTERS
+def get_seeds():
+
+	return seeds
+
+###################################################################################################
+
 print "Loading the Image ..."
 img = Image.open("img1.jpg")
 img.load()
-img_data = np.array(img)
+img = np.array(img)
 
-# Height and Width of the Image
-height = float(len(img_data))
-width = float(len(img_data[0]))
+img_height = len(img)					# Dimensions of the image
+img_width = len(img[0])					#
 
-# Constructing the NORMALIZED matrix of the image
-print "Constructing the mormalized matrix of the Image ..."
-i = 0
-data = []
-for row in img_data:
-	j = 0
-	for pixel in row:
-		data.append([i, j, pixel[0], pixel[1], pixel[2]])
-		j = j + 1
-	i = i + 1
+print "Constructing the feature matrix ..."
+data = get_data(img)
 
-# Data is a numpy array of NORMALIZED [x,y,r,g,b] values of pixels in the image
-# The dimension of this array with be h*w*5
-data = np.array(data)
+print "Estimating the bandwidth of the Kernel ..."
+bandwidth = estimate_bandwidth(data, quantile=0.3, n_samples=None, random_state=0)
 
-###################################################################################################
+print "Getting the initial kernel seeds ..."
+seeds = get_bin_seeds(data, bandwidth, 2)
+

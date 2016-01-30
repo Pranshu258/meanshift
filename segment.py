4bfd5ec93656cb12b67c80e517a9a28c0deab5cb
Pranshu Gupta <pranshug@iitk.ac.in>
2016-01-31 02:04:05 +0530

    Made some modifications

--- segment.py
+++ segment.py
@@ -10,25 +10,25 @@ from gaussian_mean import gaussian_mean
 
 ###################################################################################################
 bandwidth = None
-Bin = None
+Bin = 40
 kertype = "flat"
 
-if len(sys.argv) == 4:
+
+if len(sys.argv) == 3:
 	bandwidth = int(sys.argv[1])
-	Bin = int(sys.argv[2])
-	gaussian = int(sys.argv[3])
+	gaussian = int(sys.argv[2])
 else:
-	print "Usage: python segment.py bandwidth seeding_bin_size gaussian"
+	# print "Usage: python segment.py bandwidth do_gaussian"
 	exit()
 
 if gaussian == 1:
 	kertype = "gaussian"
 
-m = 0.5
+m = 1
 S = 5
 threshold = 1.0
 
-print "Loading the Image ..."
+# print "Loading the Image ..."
 img = Image.open("img/input.jpg")
 img.load()
 img = np.array(img)
@@ -37,19 +37,18 @@ seg_img = img
 
 rows, cols, dim = img.shape
 
-seg_meandist = np.array([[1000.0 for r in xrange(cols)] for c in xrange(rows)])
 meandist = np.array([[1000.0 for r in xrange(cols)] for c in xrange(rows)])
 labels = np.array([[-1 for r in xrange(cols)] for c in xrange(rows)])
 
-print "Running Mean Shift Algorithm ..."
-# Get the kernel for a point
+# print "Running the Mean Shift algorithm ..."
+
 start = t.time()
 
 means = []
 for r in xrange(0,rows,Bin):
 	for c in xrange(0,cols,Bin):
 		seed = np.array([r,c,img[r][c][0],img[r][c][1],img[r][c][2]])
-		for n in xrange(10):
+		for n in xrange(15):
 			x = seed[0]
 			y = seed[1]
 			r1 = max(0,x-bandwidth*5)
@@ -64,11 +63,8 @@ for r in xrange(0,rows,Bin):
 					D = np.linalg.norm([dc,ds])
 					if D < bandwidth:
 						kernel.append([i,j,img[i][j][0],img[i][j][1],img[i][j][2]])
-						meandist[i][j] = D
-			kernel = np.array(kernel)
-			# Get the mean of the kernel which will be used as the new seed
-			
-			# Kernel
+			kernel = np.array(kernel)			
+
 			if gaussian == 0:
 				mean = np.mean(kernel,axis=0,dtype=np.int64)
 			elif gaussian == 1:
@@ -76,35 +72,57 @@ for r in xrange(0,rows,Bin):
 
 			# Get the shift
 			dc = np.linalg.norm(seed[2:] - mean[2:])
-			ds = (np.linalg.norm(mean[:2] - mean[:2]))*m/S
+			ds = (np.linalg.norm(seed[:2] - mean[:2]))*m/S
 			dsm = np.linalg.norm([dc,ds])
 			seed = mean
 			if dsm <= threshold:
+				# print "Mean " + str(len(means)+1) + " converges in: " + str(n) + " iterations"
 				break
 		means.append(seed)
 
-		# Check if this seed is better than the last assigned color for the pixels in kernel
-		for k in kernel:
-			i = k[0]
-			j = k[1]
-			D = np.linalg.norm([meandist[i][j],dsm])
-			if D < seg_meandist[i][j]:
-				seg_meandist[i][j] = D
-				seg_img[i][j] = mean[2:]
-				labels[i][j] = len(means) - 1
-
 end = t.time()
 
+# print "Grouping together the means that are closer than the bandwidth ..."
+flags = [1 for me in means]
+for i in xrange(len(means)):
+	if flags[i] == 1:
+		w = 1.0
+		j = i + 1
+		while j < len(means):
+			dc = np.linalg.norm(means[i][2:] - means[j][2:])
+			ds = (np.linalg.norm(means[i][:2] - means[j][:2]))*m/S
+			dsm = np.linalg.norm([dc,ds])
+			if dsm < bandwidth:
+				means[i] = means[i] + means[j]
+				w = w + 1.0
+				flags[j] = 0
+			j = j + 1
+		means[i] = means[i]/w
+converged_means = []
+for i in xrange(len(means)):
+	if flags[i] == 1:
+		converged_means.append(means[i])
+converged_means = np.array(converged_means)
 
+# # print "Constructing the segmented image ..."
+# for i in xrange(rows):
+# 	for j in xrange(cols):
+# 		for c in xrange(len(converged_means)):
+# 			dc = np.linalg.norm(img[i][j] - converged_means[c][2:])
+# 			ds = (np.linalg.norm(np.array([i,j]) - converged_means[c][:2]))*m/S
+# 			D = np.linalg.norm([dc,ds])
+# 			if D < meandist[i][j]:
+# 				meandist[i][j] = D
+# 				labels[i][j] = c
+# 		seg_img[i][j] = converged_means[labels[i][j]][2:] 
 
-print "Saving the segmented image ..."
-img = Image.fromarray(seg_img)
-img.save("img/" + kertype + "_output_" + str(bandwidth) + "_" + str(Bin) + ".jpg")
+# print "Saving the segmented image ..."
+# seg_img = Image.fromarray(seg_img)
+# seg_img.save("img/" + kertype + "_output_" + str(bandwidth) + ".jpg")
 
-print "Time taken for segmentation: " + str((end - start)/60) + " min"
-print "Bandwidth: " + str(bandwidth)
-print "Seeds Bin Size: " + str(Bin)
-print "Number of Seeds: " + str(len(means))
-print "Number of means after convergence: "
-print "Relative weight given to spatial distance (m): " + str(m)
-print "Spatial Distance Normalization Parameter (S): " + str(S)
+# print "Time taken for segmentation: " + str((end - start)/60) + " min"
+# print "Bandwidth: " + str(bandwidth)
+# print "Number of Means before convergence: " + str(len(means))
+# print "Number of Means after convergence: " + str(len(converged_means))
+
+print bandwidth, len(converged_means)
